74ac5a72b8e3183f412a26a38b5376dc751610e8
Akira Matsuda <akira.matsuda@mopera.net>
2017-12-14 14:28:58 +0900

    繰り返し処理追加

--- sample1/src/com/matu/sample/action/SampleAction.java
+++ sample1/src/com/matu/sample/action/SampleAction.java
@@ -3,6 +3,11 @@ package com.matu.sample.action;
 public class SampleAction {
 
 	public String exectute() {
+
+		for (int i = 0; i < 100; i++) {
+			System.out.println(i);
+		}
+
 		return "Return";
 	}
 }
